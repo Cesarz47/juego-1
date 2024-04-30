e6155391f786a72a376a9e7b819a256d8ed1a5bc
Cesarz47 <168601510+Cesarz47@users.noreply.github.com>
2024-04-30 18:03:48 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ images.g.ts
@@ -0,0 +1,65 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Pikachu":return img`
+..................
+..................
+..................
+..f............f..
+..ff..........ff..
+..fff........fff..
+..ffffff..ffffff..
+...ff55ffff55ff...
+....f55555555f....
+...ff5ff55ff5ff...
+..ff55ff55ff55ff..
+.ff525555555525ff.
+.f522255ff552225f.
+.ff525f5555f525ff.
+..ff555ffff555ff..
+...ff55555555ff...
+....ffffffffff....
+..................
+`;
+            case "image2":
+            case "Rayo 1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f f . . . 
+. . . . f f 5 5 5 5 5 f f . . . 
+. . . f f 5 5 5 5 5 f f . . . . 
+. . f f 5 5 5 5 5 f f f f f f . 
+. f f 5 5 5 5 5 5 5 5 5 5 5 f . 
+. f f f f f f f f f 5 5 5 f f . 
+. . . . . . . . f f 5 5 f f . . 
+. . . . . . . f f 5 f f f . . . 
+. . . . . . f f 5 f f . . . . . 
+. . . . . . f 5 f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
